0854e347004db88db060b047afd9ae95c01681bb
Jacob Graham <jacobgraham2025@u.northwestern.edu>
2025-05-20 16:10:34 -0500

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    author='Jacob Graham',
+    description="A package for analyzing full atomistic simulations of mussel foot protein 5 using MDAnalysis.",
+    name='crosslinked_mfp_analysis',
+    version='0.1.0'
+)
