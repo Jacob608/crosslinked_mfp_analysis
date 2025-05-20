e8d8611e68db1cc79514f14503314b792104ee0e
Jacob Graham <jacobgraham2025@u.northwestern.edu>
2025-05-20 13:25:56 -0500

    Add content to __init__.py

---
+++ crosslinked_mfp_analysis/__init__.py
@@ -0,0 +1,6 @@
+"""
+crosslinked_mfp_analysis
+
+This package provides tools for analyzing covalent bonds, ion coordination,
+and molecular surroundings in crosslinked mussel foot protein simulations.
+"""
